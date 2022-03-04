99be5c1034778fb0e08e7a016b232885f043acfc
Jeremy Jongsma <jjongsma@gmail.com>
2022-03-04 16:34:08 -0600

    Updated package versions

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,9 @@
-// Generated on 2015-12-23 using generator-angular-fullstack 3.1.1
 'use strict';
 
 var _ = require('lodash');
 var del = require('del');
 var gulp = require('gulp');
-var path = require('path');
 var gulpLoadPlugins = require('gulp-load-plugins');
-var lazypipe = require('lazypipe');
-var runSequence = require('run-sequence');
 
 var plugins = gulpLoadPlugins();
 var config;
@@ -24,73 +20,34 @@ const paths = {
  * Reusable pipelines
  ********************/
 
-let lintScripts = lazypipe()
-    .pipe(plugins.jshint, 'lib/.jshintrc')
-    .pipe(plugins.jshint.reporter, 'jshint-stylish');
-
 /********************
  * Tasks
  ********************/
-
-gulp.task('clean', () => del(['dist/**']));
-
-gulp.task('copy', () => {
-  return gulp.src([
-    'package.json',
-    'lib'
-  ], { cwdbase: true })
-    .pipe(gulp.dest(paths.dist));
-});
-
-gulp.task('lint', () => {
-    return gulp.src(_.union(paths.scripts/*, paths.tests*/))
-        .pipe(lintScripts());
-});
-
-gulp.task('test', (cb) => {
-  return runSequence(
-      //'env:all',
-      //'env:test',
-      'mocha',
-      //'mocha:coverage',
-      cb);
-});
 
 function test(files) {
   return gulp.src(files)
     .pipe(plugins.mocha({
       reporter: 'spec',
       require: [ './test/mocha.conf' ]
-    }))
-    .once('end', function() {
-      process.exit();
-    });
+    }));
 }
 
-gulp.task('mocha', () => {
-  return test(paths.tests);
-});
+gulp.task('clean', () => del([paths.dist + '/**']));
 
-gulp.task('mocha:unit', () => {
-  return test(paths.unitTests);
-});
+gulp.task('lint', () => gulp.src(_.union(paths.scripts))
+    .pipe(plugins.jshint('lib/.jshintrc'))
+    .pipe(plugins.jshint.reporter( 'jshint-stylish'))
+  );
 
-gulp.task('mocha:integration', () => {
-  return test(paths.integrationTests);
-});
+gulp.task('copy', () => gulp.src(['package.json', 'lib'], { cwdbase: true })
+    .pipe(gulp.dest(paths.dist))
+  );
 
-gulp.task('watch', () => {
-  plugins.watch(_.union(paths.scripts, paths.tests))
-    .pipe(plugins.plumber())
-    .pipe(lintScripts());
-});
+gulp.task('mocha', () => test(paths.tests));
+gulp.task('mocha:unit', () => test(paths.unitTests));
+gulp.task('mocha:integration', () => test(paths.integrationTests));
 
-gulp.task('build', (cb) => {
-  runSequence(
-    'clean',
-    'lint',
-    'copy',
-    cb);
-});
+gulp.task('build', gulp.series('clean', 'lint', 'copy'));
+gulp.task('test', gulp.series('mocha'));
 
-gulp.task('default', ['build']);
+gulp.task('default', gulp.series('build'));
